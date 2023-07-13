46178d3736991a13eb820e9e173ec694988ac561
ABHISHEK MISHRA <127968392+2sevenabhi@users.noreply.github.com>
2023-07-13 11:36:41 +0530

    Time: 24 ms (64.89%), Space: 13.8 MB (60.39%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+       int N=numCourses;
+			    vector<int> adj[N];
+
+	   for(auto i: prerequisites){
+	       adj[i[0]].push_back(i[1]);
+	   }
+	   vector<int> in(N,0);
+	   for(int i=0;i<N;i++){
+	      for(auto j: adj[i]){
+	       in[j]++;
+	   }  
+	   }
+	   
+	   queue<int> q;
+	   for(int i=0;i<N;i++){
+	       if(in[i]==0) q.push(i);
+	   }
+	   int c=0;
+	   while(!q.empty()){
+	       int n=q.front();
+	       q.pop();
+	       c++;
+	       for(auto i: adj[n]){
+	           in[i]--;
+	           if(in[i]==0) q.push(i);
+	       }
+	       
+	   }
+	   if(c==N) return 1;
+	   return 0;
+    }
+};
