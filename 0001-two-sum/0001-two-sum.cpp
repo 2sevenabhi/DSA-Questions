a0c6fb8974ee44eb8e9ebf438aceefcd14ec9134
ABHISHEK MISHRA <127968392+2sevenabhi@users.noreply.github.com>
2024-02-16 00:10:30 +0530

    Time: 8 ms (81.27%), Space: 14.1 MB (12.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,9 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
        unordered_map<int,int>mpp;
        
-        // for(auto it:nums){
-        //     mpp[it]++;
-        // }
+       
         for(int i=0;i<nums.size();i++){
             int remaining=target-nums[i];
             while(mpp.find(remaining)!=mpp.end()) {
